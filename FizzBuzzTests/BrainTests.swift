470023f5778a225d2d8412227bdaee6b3df45675
chloeverity <chloeverity@hotmail.co.uk>
2018-11-02 11:25:39 +0000

    refactoring tests

--- FizzBuzzTests/BrainTests.swift
+++ FizzBuzzTests/BrainTests.swift
@@ -10,6 +10,8 @@ import XCTest
 @testable import FizzBuzz
 
 class BrainTests: XCTestCase {
+    
+    let brain = Brain()
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -20,38 +22,31 @@ class BrainTests: XCTestCase {
     }
 
     func testIsDivisibleByThree() {
-        let brain = Brain()
         let result = brain.isDivisibleByThree(number: 3)
         XCTAssertEqual(result, true)
     }
     
     func testIsNotDivisibleByThree() {
-        let brain = Brain()
         let result = brain.isDivisibleByThree(number: 1)
         XCTAssertEqual(result, false)
     }
     
-    
     func testIsDivisibleByFive() {
-        let brain = Brain()
         let result = brain.isDivisibleByFive(number: 5)
         XCTAssertEqual(result, true)
     }
     
     func testIsNotDivisibleByFive() {
-        let brain = Brain()
         let result = brain.isDivisibleByFive(number: 1)
         XCTAssertEqual(result, false)
     }
 
     func testIsDivisibleByFifteen() {
-        let brain = Brain()
         let result = brain.isDivisibleByFifteen(number: 15)
         XCTAssertEqual(result, true)
     }
     
     func testIsNotDivisibleByFifteen() {
-        let brain = Brain()
         let result = brain.isDivisibleByFifteen(number: 1)
         XCTAssertEqual(result, false)
     }
